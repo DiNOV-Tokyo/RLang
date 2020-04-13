7667fa4beca454751eb0bfc1e696a11293f12927
DiNOV-Tokyo <dddd3ddd@gmail.com>
2020-04-14 07:22:30 +0900

    2020.04.14 add Norm dist plot

--- PlotTest.R
+++ PlotTest.R
@@ -13,16 +13,21 @@ library(ggplot2)
 dens <- density(iris$Sepal.Width)
 bw <- diff(range(iris$Sepal.Width))/20
 
+iris$t_dat<-dt(iris$Sepal.Width+mean(iris$Sepal.Width), length(iris$Sepal.Width)-1)
+iris$norm_dat<-dnorm(iris$Sepal.Width, mean=mean(iris$Sepal.Width), sd=sd(iris$Sepal.Width))
+
 ggplot(iris, aes(x=Sepal.Width)) +
   geom_histogram(aes(y=..density..), binwidth=bw, fill='white', color='black') +
   geom_density(fill='black', alpha=0.3) +
+  geom_line(aes(y=iris$norm_dat)) +
   xlim(range(dens$x))
 
 
 
-q <- qt(c(0.025, 0.975), length(iris$Sepal.Width))
+#q <- qt(c(0.025, 0.975), length(iris$Sepal.Width))
 
-p <- ggplot()
-p <- p + geom_path(data=data.frame(X=x<-seq(-4,4,len=101), Y=dt(x, length(iris$Sepal.Width))), aes(x=X, y=Y))
-p <- p + geom_ribbon(data=data.frame(X=x<-seq(q[1],q[2],len=101), Y=dt(x, length(iris$Sepal.Width))), aes(x=X, y=Y, ymin=0, ymax=Y))
-print(p)
+#p <- ggplot()
+#p <- p + geom_path(data=data.frame(X=x<-seq(-4,4,len=101), Y=dt(x, length(iris$Sepal.Width))), aes(x=X, y=Y))
+#p <- p + geom_ribbon(data=data.frame(X=x<-seq(q[1],q[2],len=101), Y=dt(seq(q[1],q[2],len=101), length(iris$Sepal.Width))), aes(x=X, y=Y, ymin=0, ymax=Y))
+#p <- p + geom_ribbon(data=data.frame(X=x<-seq(q[1],q[2],len=101), Y=dt(x, length(iris$Sepal.Width))), aes(x=X, y=Y, ymin=0, ymax=Y))
+#print(p)

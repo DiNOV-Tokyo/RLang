244a3114394b61a1f38a9a7fe999322ed653e3f4
DiNOV-Tokyo <dddd3ddd@gmail.com>
2020-04-13 21:54:37 +0900

    2020.04.13 add T.Test

--- PlotTest.R
+++ PlotTest.R
@@ -1,9 +1,11 @@
 library("datasets")
 data("iris")
-hist(iris$Sepal.Width)
-shapiro.test(x=iris$Sepal.Width)
 
 hist(iris$Petal.Width)
 shapiro.test(x=iris$Petal.Width)
+
+hist(iris$Sepal.Width)
+shapiro.test(x=iris$Sepal.Width)
+t.test(iris$Sepal.Width, mu=2.99)
 
 plot(iris[,1:4])

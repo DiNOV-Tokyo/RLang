db9176eea6a5fca7534193278139fd13d53a4faa
DiNOV-Tokyo <dddd3ddd@gmail.com>
2020-04-18 17:38:46 +0900

    2020.04.18 newly created : Chisq_test.R

---
+++ test/Chisq_test.R
@@ -0,0 +1,14 @@
+dat<-data.frame("Died"=c(0, 1, 2, 3, 4, 5, 6), "Days"=c(55, 144, 140, 95, 45, 15, 6))
+x<-dat$Died
+mu<-dat$Died %*% dat$Days / 500
+
+dat$E_Days<-sapply(x, function(x) {(ppois(x, mu)-ppois(x-1, mu))*500})
+dat$E_Days[7]<-(1-ppois(5, mu))*500
+dat
+
+layout(matrix(1:2, 1, 2))
+barplot(dat$Days, main="日数", names.arg = dat$Died)
+barplot(dat$E_Days, main="日数（期待度数）",names.arg = dat$Died)
+
+chisq.test(dat$Days,dat$E_Days) 
+

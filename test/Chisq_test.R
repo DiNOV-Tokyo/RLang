5b07bcd18e3b223ae877b26cf9646093fae684fa
DiNOV-Tokyo <dddd3ddd@gmail.com>
2020-04-21 19:55:25 +0900

    2020.04.21 correction : Chisq_test

--- test/Chisq_test.R
+++ test/Chisq_test.R
@@ -2,13 +2,12 @@ dat<-data.frame("Died"=c(0, 1, 2, 3, 4, 5, 6), "Days"=c(55, 144, 140, 95, 45, 15
 x<-dat$Died
 mu<-dat$Died %*% dat$Days / 500
 
-dat$E_Days<-sapply(x, function(x) {(ppois(x, mu)-ppois(x-1, mu))*500})
-dat$E_Days[7]<-(1-ppois(5, mu))*500
+dat$E_Days<-sapply(x, function(x) {(ppois(x, mu)-ppois(x-1, mu))})
+dat$E_Days[7]<-(1-ppois(5, mu))
 dat
 
 layout(matrix(1:2, 1, 2))
 barplot(dat$Days, main="日数", names.arg = dat$Died)
-barplot(dat$E_Days, main="日数（期待度数）",names.arg = dat$Died)
+barplot(dat$E_Days, main="日数（期待値の確率密度）",names.arg = dat$Died)
 
-chisq.test(dat$Days,dat$E_Days) 
-
+chisq.test(dat$Days,p=dat$E_Days) 
